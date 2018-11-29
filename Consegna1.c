8b3a71c74d3b7e52f082c1856efe4fd948c0d5bf
CannavoMichele <uni271155@studium.unict.it>
2018-11-29 22:07:01 +0100

    Consegna1 v0.1
    Implementati i test per gli argomenti ed il test per la cartella

---
+++ Consegna1.c
@@ -0,0 +1,90 @@
+//==============================================================================
+// Cannavo' Michele 046002210
+//==============================================================================
+// Progettino - V1.0
+//
+// Consegna 1
+//==============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h> // stat()
+#include <errno.h>
+
+//// Variabili globali, non se serviranno come tali
+
+// Numero di core del processore
+long nCore=0;
+
+// Cartella di lavoro inizzializzata su HOME 
+char *workPath="\\HOME";
+
+int getNCore(long*);
+int testPath(char*);
+
+int main(int argc,char* argv[])
+{
+    
+// Testo che ci sia il path come argomento,
+// in caso di altri argomenti modifico. 
+    if (argc!=2)
+    {
+        errno=EINVAL;
+        perror("Argomenti non validi\n");
+        exit(EXIT_FAILURE);
+    }
+    
+// Test Cartella
+    if(testPath(argv[1]))
+    {
+        printf("Cartella Trovata\n");
+    }
+    else
+    {
+        printf("Cartella non trovata\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    if(getNCore(&nCore))    
+    {
+        
+        printf("Rilevati %ld Core\n",nCore);
+    }
+    else
+    {
+        printf("Impossibile reperire numero di core\n");
+        exit(EXIT_FAILURE);
+    }
+    
+
+    return(EXIT_SUCCESS);
+}
+
+
+/// @func getNCore
+/// @brief La funzione legge dal file /proc/cinfo l'informazione richiesta
+/// @return Numero di core del processore
+int getNCore(long *nCore)
+{
+// Per test, da implementare con la lettura del file apposito
+    *nCore=2;
+}
+
+/// @func getNCore()
+/// @param stringa contenete il Path
+int testPath(char* wPath)
+{
+struct stat path;
+
+    if (stat(wPath, &path) == 0 && S_ISDIR(path.st_mode))
+    {
+        return(EXIT_SUCCESS);
+    }
+    else
+    {
+        return(EXIT_FAILURE);
+    }
+
+}
+
