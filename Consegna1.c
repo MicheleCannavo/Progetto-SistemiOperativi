d61388aadd669ffbe59f18044398204a433421b9
CannavoMichele <uni271155@studium.unict.it>
2018-11-30 17:15:29 +0100

    Consegna1 v0.3
    Aggiunta la modifica della cartella corrente

--- Consegna1.c
+++ Consegna1.c
@@ -1,7 +1,10 @@
 //==============================================================================
 // Cannavo' Michele 046002210
+// Anno 2018/2019
+// Sistemi Operativi
 //==============================================================================
 // Progettino - V1.0
+// Licenza GPL 3.0
 //
 // Consegna 1
 //==============================================================================
@@ -9,23 +12,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/stat.h> // stat()
 #include <errno.h>
+#include <dirent.h> // getwd(), chdir()
 
-//// Variabili globali, non se serviranno come tali
-
-// Numero di core del processore
+///// Variabili globali /////
 long nCore=0;
+char workPath[PATH_MAX]="~/";
 
-// Cartella di lavoro inizzializzata su HOME 
-char *workPath="\\HOME";
-
+///// PROTOTIPI ////
 int getNCore(long*);
 int testPath(char*);
 
+#define DEBUG_ 
+
 int main(int argc,char* argv[])
 {
-    
+// Stringa per memorizzare l'attuale directory
+    char* currDir=NULL;
+
 // Testo che ci sia il path come argomento,
 // in caso di altri argomenti modifico. 
     if (argc!=2)
@@ -34,15 +40,28 @@ int main(int argc,char* argv[])
         perror("Argomenti non validi\n");
         exit(EXIT_FAILURE);
     }
-    
-// Test Cartella
+
+#ifdef DEBUG_
+printf("directory attuale= [%s]\n",getcwd(NULL,0));
+#endif
+ 
+// Test directory
     if(!testPath(argv[1]))
-    {
-        printf("Cartella Trovata\n");
+    {  
+        printf("directory di lavoro= [%s]\nMi sposto su di essa...\n",argv[1]);  
+        if( (currDir=getcwd(NULL,0))==0 ) // Salvo l'attuale directory e...
+            return(EXIT_FAILURE);
+                
+        if( chdir(argv[1]) )
+            return(EXIT_FAILURE);        // ..mi sposto in quella di lavoro...
+       
+       strncpy(workPath, argv[1], PATH_MAX);
+       printf("Nuova directory di lavoro= [%s]\n",workPath); 
     }
+    
     else
     {
-        printf("Cartella non trovata\n");
+        printf("directory non trovata\n");
         exit(EXIT_FAILURE);
     }
     
@@ -56,12 +75,24 @@ int main(int argc,char* argv[])
         printf("Impossibile reperire numero di core\n");
         exit(EXIT_FAILURE);
     }
+        
+
+    if( chdir( currDir ) )
+        return(EXIT_FAILURE); // Ritorno alla directory iniziale
+        
+#ifdef DEBUG_
+printf("Ritorno alla directory =%s\n",getcwd(NULL,0));
+#endif
     
+    free( currDir );      // Libero la memoria allocata con getcwd()
+    currDir = NULL;         
+
 
     return(EXIT_SUCCESS);
 }
 
-
+//==============================================================================
+//==============================================================================
 /// @func getNCore
 /// @brief La funzione legge dal file /proc/cinfo l'informazione richiesta
 /// @return Numero di core del processore
